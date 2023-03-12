e5227558263282d39b5aca43ba80c159cd98e854
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-12 23:44:17 +0100

    made changes so that container and a button element are going before grid-container

--- script.js
+++ script.js
@@ -4,13 +4,25 @@
 const container = document.querySelector('.container');
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
-container.appendChild(gridContainer);
-
-// button-container and a button element //
 const buttonContainer = document.createElement('div');
 buttonContainer.classList.add('button-container');
 container.appendChild(buttonContainer);
+container.appendChild(gridContainer);
+
+// button-container and a button element //
+
 const changeGridButton = document.createElement('button');
 changeGridButton.classList.add('button-grid')
 buttonContainer.appendChild(changeGridButton);
 changeGridButton.textContent = "CHANGE GRID";
+
+// function for the grid size //
+
+function gridSize(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+
+}
+
+gridSize(16)
