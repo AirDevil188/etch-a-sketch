28e71b3bdf0d2c2787ec8ef4db8f8e515d0c3a34
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-13 00:13:40 +0100

    added if loop so that the user can't enter the value greater than 100 for the grid size

--- script.js
+++ script.js
@@ -1,64 +1,72 @@
 /* DOM */
 
 // grid-container //
-const container = document.querySelector('.container');
-const gridContainer = document.createElement('div');
-gridContainer.classList.add('grid-container');
-const buttonContainer = document.createElement('div');
-buttonContainer.classList.add('button-container');
+const container = document.querySelector(".container");
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container");
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("button-container");
 container.appendChild(buttonContainer);
 container.appendChild(gridContainer);
 
 // button-container and a button element //
 
-const changeGridButton = document.createElement('button');
-changeGridButton.classList.add('button-grid')
+const changeGridButton = document.createElement("button");
+changeGridButton.classList.add("button-grid");
 buttonContainer.appendChild(changeGridButton);
 changeGridButton.textContent = "CHANGE GRID";
 
 /* DOM */
 
-changeGridButton.addEventListener('click', () => {
-    userDefinedGrid();
-})
-
+changeGridButton.addEventListener("click", () => {
+  userDefinedGrid();
+});
 
 // function that creates the grid based on the input of the user //
 
 function userDefinedGrid() {
-    let number = prompt("Please select the size of your grid between 1-100");
+  let number = prompt("Please select the size of your grid between 1-100.");
+  if (number < 101) {
     gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     gridSize(number);
+  } else {
+    alert("ERROR! Please enter the number that is smaller or equal to 100.");
+  }
 }
 
 // function for the grid size //
 
 function gridSize(size) {
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-// for loop for creating squares //
+  // for loop for creating squares //
 
-    for (let i = 0; i < size * size; i++) {
-        const squares = document.createElement('div');
-        gridContainer.appendChild(squares);
+  for (let i = 0; i < size * size; i++) {
+    const squares = document.createElement("div");
+    gridContainer.appendChild(squares);
 
-        // Event Listener for changing background color of squares when hovering //
-        squares.addEventListener('mouseover', () => {
-            squares.setAttribute('style', 'background-color: red; border: 3px solid black')
-        })
+    // Event Listener for changing background color of squares when hovering //
+    squares.addEventListener("mouseover", () => {
+      squares.setAttribute(
+        "style",
+        "background-color: red; border: 3px solid black"
+      );
+    });
 
-        // function that resets the grid //
+    // function that resets the grid //
 
-        function clearGrid() {
-            changeGridButton.addEventListener('click', () => {
-                squares.removeAttribute('style', 'background-color: red; border: 3px solid black')
-            })
-        }
-        clearGrid();
+    function clearGrid() {
+      changeGridButton.addEventListener("click", () => {
+        squares.removeAttribute(
+          "style",
+          "background-color: red; border: 3px solid black"
+        );
+      });
     }
-    
+    clearGrid();
+  }
 }
 
-gridSize(16)
+gridSize(16);
