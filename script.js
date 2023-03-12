56087a69ac4e2a536caa94938105062ad500587b
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-12 23:19:26 +0100

    added a container for grid system

---
+++ script.js
@@ -0,0 +1,7 @@
+/* DOM */
+
+// grid-container //
+const container = document.querySelector('.container');
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('grid-container');
+container.appendChild(gridContainer);
