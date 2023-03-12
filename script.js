20bc5172b135bd78ed146b7b942e55cc289cbb30
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-12 23:25:04 +0100

    added button container, button element to change size of the grid to the DOM

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const container = document.querySelector('.container');
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('grid-container');
 container.appendChild(gridContainer);
+
+// button-container and a button element //
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container');
+container.appendChild(buttonContainer);
+const changeGridButton = document.createElement('button');
+buttonContainer.appendChild(changeGridButton);
+changeGridButton.textContent = "CHANGE GRID"
